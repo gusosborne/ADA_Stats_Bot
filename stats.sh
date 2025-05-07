47cb1a795e6c29c0331d9c92c58ed4e6dc3ef444
ASTCH <gusosborne@gmail.com>
2025-05-06 21:27:16 -0500

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 ##Cardano Node Block Stats (for Guild Operators environments)
-##This is terribly inneficcient. You should probably not use this, but write your own using arrays. I am not a dev.
+##Data is gathered from mostly local data, using Guild Operators scripts (cncli/cardano-cli/koios)
+##This is quite inneficcient. You should probably not use this, but write your own using less queries and arrays. I am not a dev.
 ##Gus
+
 BLOCKLOG_DB="/opt/cardano/cnode/guild-db/blocklog/blocklog.db"
 current_epoch=$(cardano-cli query tip --mainnet|grep epoch|awk -F ' ' '{print $2}'|sed 's/,//g')
 total=$(sqlite3 "${BLOCKLOG_DB}" "SELECT * FROM blocklog WHERE epoch=$current_epoch;"|wc -l)
@@ -11,7 +13,7 @@ leader=$(sqlite3 "${BLOCKLOG_DB}" "SELECT * FROM blocklog WHERE epoch=$current_e
 stolen=$(sqlite3 "${BLOCKLOG_DB}" "SELECT * FROM blocklog WHERE epoch=$current_epoch;"|grep -e ghosted -e stolen -e missed|wc -l)
 adopted=$(sqlite3 "${BLOCKLOG_DB}" "SELECT * FROM blocklog WHERE epoch=$current_epoch;"|grep adopted|wc -l)
 next_epoch_blocks=$(sqlite3 "${BLOCKLOG_DB}" "SELECT * FROM blocklog WHERE epoch=$next_epoch;"|grep leader|wc -l)
-livestake=$(curl -sSL -f -X POST -H "Content-Type: application/json" -d '{"_pool_bech32_ids":["pool1q2fh3cl6rx0wv5gry4qx5l4h65qpjf7x99xmq66nqj2fj5g6u9z"]}' "https://api.koios.rest/api/v1/pool_info"|awk -F "," '{print $43}'|awk -F '"' '{print $4}'| rev | cut -c7- | rev|numfmt --grouping)
+livestake=$(curl -sSL -f -X POST -H "Content-Type: application/json" -d '{"_pool_bech32_ids":["pool1example6rx0wv5gry4qx5l4h65qpjf7x99xmq66nqj2fj5gzzzz"]}' "https://api.koios.rest/api/v1/pool_info"|awk -F "," '{print $43}'|awk -F '"' '{print $4}'| rev | cut -c7- | rev|numfmt --grouping)
 
 echo ""
 echo "Current Epoch: $current_epoch"
